56bd42a07ad477eba9f1618486885210469fdef1
yoen-velt <yoen@velt.dev>
2025-10-30 17:36:26 -0400

    Updated styling stucture

--- apps/react/tables/libraries/ag-grid/single-tool/app/layout.tsx
+++ apps/react/tables/libraries/ag-grid/single-tool/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Urbanist } from 'next/font/google'
 import '../styles/globals.css'
+import '../ui-customization/styles.css'
 import { AppProviders } from "@/app/userAuth/AppProviders";
 
 const urbanist = Urbanist({
