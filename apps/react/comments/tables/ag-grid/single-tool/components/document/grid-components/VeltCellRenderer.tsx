689c6d5faf862269fc6fc1c3e52c384ea3eff777
yoen-velt <yoen@velt.dev>
2025-11-04 20:48:14 -0500

    Fixed ag-grid single-tool

--- apps/react/comments/tables/ag-grid/single-tool/components/document/grid-components/VeltCellRenderer.tsx
+++ apps/react/comments/tables/ag-grid/single-tool/components/document/grid-components/VeltCellRenderer.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { VeltCommentTool, VeltCommentBubble } from '@veltdev/react';
 import { CellFormatting } from '../types';
 import { getCellFormattingKey } from '../utils';
 
@@ -8,7 +7,6 @@ export const createVeltCellRenderer = (
   documentId: string | null
 ) => {
   const VeltCellRenderer = (props: any) => {
-  const cellId = `cell-${props.data.id}-${props.colDef.field}`;
   const cellKey = getCellFormattingKey(props.data.id, props.colDef.field);
   const formatting = cellFormatting[cellKey] || {};
 
@@ -24,10 +22,6 @@ export const createVeltCellRenderer = (
   return (
     <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '100%', height: '100%' }}>
       <span style={{ ...textStyle, paddingLeft: '12px' }}>{props.value}</span>
-      <div style={{ display: 'flex', alignItems: 'center', gap: '4px', paddingRight: '8px' }}>
-        <VeltCommentTool targetElementId={cellId} />
-        <VeltCommentBubble targetElementId={cellId} />
-      </div>
     </div>
   );
   };
