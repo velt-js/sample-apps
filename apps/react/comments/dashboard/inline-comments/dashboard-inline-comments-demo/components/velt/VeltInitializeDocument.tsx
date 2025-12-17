1f3dc3172b7e578b65a06ba992e446b38076f686
Rakesh <rakesh@velt.dev>
2025-12-17 01:59:06 -0800

    save

--- apps/react/comments/dashboard/inline-comments/dashboard-inline-comments-demo/components/velt/VeltInitializeDocument.tsx
+++ apps/react/comments/dashboard/inline-comments/dashboard-inline-comments-demo/components/velt/VeltInitializeDocument.tsx
@@ -12,7 +12,6 @@ export default function VeltInitializeDocument() {
   // [Velt] Set document in Velt. This is the resource where all Velt collaboration data will be scoped.
   useEffect(() => {
     if (!documentId || !documentName) return;
-    console.log("Setting document", documentId, documentName);
 
     setDocuments([
       { id: documentId, metadata: { documentName: documentName } },
