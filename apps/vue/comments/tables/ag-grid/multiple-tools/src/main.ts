47555eda29d95b520816ad34a543e9bcd43c78ca
yoen-velt <yoen@velt.dev>
2026-03-05 19:47:57 -0500

    Cleanup Pt 2.

--- apps/vue/comments/tables/ag-grid/multiple-tools/src/main.ts
+++ apps/vue/comments/tables/ag-grid/multiple-tools/src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './styles/globals.css';
-import './components/velt/ui-customization/styles.css';
 
 const app = createApp(App);
 app.mount('#app');
