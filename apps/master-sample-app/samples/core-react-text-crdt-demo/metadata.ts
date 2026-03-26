31a4f656aef2a3cb5f93be0376040c24812b7d6a
yoen-velt <yoen@velt.dev>
2026-03-26 01:03:17 -0400

    New Vercel Links for master app

--- apps/master-sample-app/samples/core-react-text-crdt-demo/metadata.ts
+++ apps/master-sample-app/samples/core-react-text-crdt-demo/metadata.ts
@@ -5,7 +5,7 @@ const metadata: SampleMetadata = {
   title: 'React / CRDT / Text Editors / Core / core-react-text-crdt-demo',
   category: 'feature',
   section: 'Core',
-  iframeUrl: 'https://sample-apps-core-react-text-crdt-demo.vercel.app',
+  iframeUrl: 'https://sample-apps-core-react-text-crdt-de.vercel.app',
   iframeUrl2: 'https://sample-apps-core-react-text-crdt-demo-velt-team-eng.vercel.app',
   githubUrl: 'https://github.com/velt-js/sample-apps/tree/main/apps/react/crdt/text-editors/core/core-react-text-crdt-demo',
   githubRepoPath: 'velt-js/sample-apps',
--- apps/master-sample-app/samples/js-core-non-react-text-crdt-demo/metadata.ts
+++ apps/master-sample-app/samples/js-core-non-react-text-crdt-demo/metadata.ts
@@ -5,7 +5,7 @@ const metadata: SampleMetadata = {
   title: 'Javascript / CRDT / Text Editors / Core / core-non-react-text-crdt-demo',
   category: 'feature',
   section: 'Core',
-  iframeUrl: 'https://sample-apps-core-non-react-crdt-dem.vercel.app',
+  iframeUrl: 'https://sample-apps-core-non-react-text-crd.vercel.app',
   iframeUrl2: 'https://sample-apps-core-non-react-text-crdt-demo-velt-team-eng.vercel.app',
   githubUrl: 'https://github.com/velt-js/sample-apps/tree/main/apps/javascript/crdt/text-editors/core/core-non-react-text-crdt-demo',
   githubRepoPath: 'velt-js/sample-apps',
